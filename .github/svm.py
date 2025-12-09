5e3122cfdd397180cf184578ceab0127e7fcb99f
Abel-00 <1709303@uab.cat>
2025-12-09 11:18:01 +0100

    canvis_svm

--- .github/svm.py
+++ .github/svm.py
@@ -1,12 +1,13 @@
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import GridSearchCV, StratifiedKFold
 import numpy as np
 import pandas as pd
 
 
 
-def calcula_prediccio_svm(X_train_df, Y_train_ser, X_test_df, C):
+def calcula_prediccio_svm(X_train_df, Y_train_ser, X_test_df, C, gamma='scale', kernel='rbf', degree = 3):
     """
     Entrena un SVM sobre els histogrames BoW i fa prediccions sobre el test.
     Utilitza GridSearchCV per buscar bons hiperparàmetres (C i gamma).
@@ -21,7 +22,7 @@ def calcula_prediccio_svm(X_train_df, Y_train_ser, X_test_df, C):
     
 
     # 4. Definim el model SVM bàsic
-    svm = SVC(kernel='rbf', C=C, class_weight='balanced', random_state=42, decision_function_shape='ovr')
+    svm = SVC(C=C, class_weight='balanced', gamma=gamma, kernel=kernel, degree=degree)
     # el rbf és el que funciona millor per a reconeixement d'imatges
     # la C és el que varia, C gran penalitza molt els errors i pot sobreajustar, C petita accepta més errors i pot generalitzar millor, però també ajustar poc
     #class_weight='balanced' el que fa és donar més pes a les classes minoritàries per evitar biaixos i menys pes a les majoritàries
@@ -49,7 +50,14 @@ def avalua_prediccions_svm(Y_test_ser, y_pred):
 
     # 1. Accuracy global
     acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred, average='weighted')
+    rec = recall_score(y_test, y_pred, average='weighted')
+    f1 = f1_score(y_test, y_pred, average='weighted')
+
     print("Accuracy:", acc)
+    print("Precision:", prec)
+    print("Recall:", rec)
+    print("F1-score:", f1)
 
     # 2. Matriu de confusió
     labels_ordenats = sorted(list(set(y_test)))  #per mantenir sempre el mateix ordre a la matriu de confusió
@@ -58,6 +66,6 @@ def avalua_prediccions_svm(Y_test_ser, y_pred):
     cm_df = pd.DataFrame(cm, index=labels_ordenats, columns=labels_ordenats)#ho passem a dataframe per poder guardar-ho millor
     
  
-    return acc, cm_df
+    return acc, cm_df, prec, rec, f1
 
 #llista dels fitxers que donen error
